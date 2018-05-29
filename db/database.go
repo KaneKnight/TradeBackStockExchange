37cbc6825df1a3bee89158efe0bc9995c07a2b04
kk2216 <kk2216@ic.ac.uk>
2018-05-29 20:25:25 +0100

    Define schema for database.

--- db/database.go
+++ db/database.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-  //"database/sql"
     "fmt"
     "log"
     "time"
@@ -9,27 +8,77 @@ import (
     "github.com/jmoiron/sqlx"
 )
 
+
+var schema = `
+create table transaction (
+    transactionId serial,
+    buyerId integer,
+    sellerId integer,
+    ticker text,
+    amountTraded integer,
+    cashTraded float(64)
+);
+
+create table user (
+    userId serial,
+    userName text,
+    userPasswordHash text
+);
+
+create table company (
+    ticker text,
+    name text
+);`
+
 type DataBase struct {
-  User string
-  Password string
-  Name string
-  Sslmode string
-  Port int
+    User string
+    Password string
+    Name string
+    Sslmode string
+    Port int
 }
 
 type Transaction struct {
-  BuyerId int
-  SellerId int
-  EquityTicker string
-  AmountTraded int
-  CashTraded  int
-  TimeOfTrade time.Time
+    TransactionId int
+    BuyerId int
+    SellerId int
+    EquityTicker string
+    AmountTraded int
+    CashTraded  float64
+    TimeOfTrade time.Time
+}
+
+/* No args, called on the DataBase struct and returns a pointer to
+ * sqlx database object.*/
+func (db DataBase) openDataBase() (*sqlx.DB, error) {
+    connStr := fmt.Sprintf(`user=%s password=%s dbname=%s sslmode=%s port=%d`,
+                           db.User,
+                           db.Password,
+                           db.Name,
+                           db.Sslmode,
+                           db.Port)
+    fmt.Println(connStr)
+    return sqlx.Open("postgres", connStr)
+}
+
+/* 5 args, takes fields of Transaction struct */
+func (db *sqlx.DB) insertTransaction(buyerId int,
+                                     sellerId int,
+                                     ticker string,
+                                     amountTraded int,
+                                     cash float64,
+                                     timeOfTrade time.Time) {
+    db
 }
 
 func main() {
-  connStr := `user=g1727122_u password=PTqnydAPoe
-    dbname=g1727122_u sslmode=require port=5432`
-  db, err := sqlx.Open("postgres", connStr)
+  database := DataBase{"g1727122_u",
+     "PTqnydAPoe",
+     "g1727122_u",
+     "require",
+     5432}
+
+  db, err := database.openDataBase()
 
   if err != nil {
 		log.Fatal(err)
